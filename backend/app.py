184ff104ff1ceac69b2822493671699f4ec89513
Muhaha ～ <muhaha@Muhahas-MacBook-Pro.local>
2020-02-13 03:00:02 -0600

    change

--- backend/app.py
+++ backend/app.py
@@ -32,6 +32,7 @@ def init_person():
         personal_info_list.append(personal_info)
     return jsonify(personal_info_list)
 
+
 @app.route('/init_time', methods=['GET'])
 def init_time():
     date = []
@@ -46,22 +47,22 @@ def init_time():
 
 @app.route('/search_gps', methods=['GET','POST'])
 def search_gps():
+    # load data from frontend post request
     firstname = request.args.get("firstname")
     lastname = request.args.get("lastname")
     time_start = request.args.get("time_start")
     time_end = request.args.get("time_end")
     time_start = datetime.strptime(time_start, "%y-%m-%d %H:%M")
     time_end = datetime.strptime(time_end, "%y-%m-%d %H:%M")
-
+    # start engine in sql datbase
     engine = create_engine(patterns_conn)
     Base = automap_base()
     Base.prepare(engine, reflect = True)
     db = sessionmaker(bind = engine)()
     Gps = Base.classes.gps
     Car_assignments = Base.classes.car_assignments
-
+    # trigger search condition
     if((len(firstname) != 0 ) and (len(lastname) != 0)):
-        # search gps data with certain name
         result_list = (db.query(Car_assignments, Gps)
                     .join(Car_assignments, Car_assignments.carid == Gps.id)
                     .filter_by(firstname = firstname)
@@ -88,61 +89,6 @@ def search_gps():
                         "longtitude" : float(result.gps.longtitude)}
             gps_record_list.append(gps_record)
     return jsonify(gps_record_list)
-    
-
-
-
-
-
-
-
-@app.route('/')
-def hello_world():
-    engine = create_engine(patterns_conn)
-    Base = automap_base()
-    Base.prepare(engine, reflect = True)
-    db = sessionmaker(bind = engine)()
-    Gps = Base.classes.gps
-    result_list = db.query(Gps).filter_by(id ="34")
-    gps_record_list = []
-    for result in result_list:
-        gps_record = {"latitude" : result.latitude, "longtitude" : result.longtitude} 
-        gps_record_list.append(gps_record)
-    return render_template('index.html', gps_record_list = gps_record_list)
-
-
-@app.route('/search/', methods=['GET','POST'])
-def search():
-    firstname = "Isia"
-    lastname = "Vann"
-    time_start = "2014-01-06 08:00"
-    time_end = "2014-01-06 17:00"
-    engine = create_engine(patterns_conn)
-    Base = automap_base()
-    Base.prepare(engine, reflect = True)
-    db = sessionmaker(bind = engine)()
-    Gps = Base.classes.gps
-    Car_assignments = Base.classes.car_assignments
-    # search gps data with certain name
-    if(firstname is not None and lastname is not None):
-        result_list = (db.query(Car_assignments, Gps)
-                    .join(Car_assignments, Car_assignments.carid == Gps.id)
-                    .filter_by(firstname = firstname)
-                    .filter_by(lastname = lastname)
-                    .filter(Gps.timestamp.between(time_start, time_end)))
-        gps_record_list = []
-        for result in result_list:
-            gps_record = {"timestamp" : result.gps.timestamp,
-                         "latitude" : result.gps.latitude, 
-                         "longtitude" : result.gps.longtitude}
-            gps_record_list.append(gps_record)
-    else:
-        result_list = db.query(Gps)
-        gps_record_list = []
-        for result in result_list:
-            gps_record = {"latitude" : result.latitude, "longtitude" : result.longtitude} 
-            gps_record_list.append(gps_record)
-    return render_template('index.html', gps_record_list = gps_record_list)
 
 
 if __name__ == "__main__":
